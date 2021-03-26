97a6a65fce540e986ad8174bbe3adeb6e5c21bf3
GowPlayTeam <55928876+GowPlay@users.noreply.github.com>
2021-03-26 15:01:04 -0500

    Update keys.js

--- src/settings/keys.js
+++ src/settings/keys.js
@@ -1,10 +1,10 @@
 module.exports = {
     database: {
-        host: 'bhbmnp7wzg0rbbgjpwgh-mysql.services.clever-cloud.com',
-        user: 'ujoc8augpd3q2rpr',
-        database: 'bhbmnp7wzg0rbbgjpwgh',
+        host: '-mysql.services.clever-cloud.com',
+        user: 'nombre de usuario',
+        database: 'nombre base de datos',
         port: '3306',
-        password: '6j31e8ByzSGd6f3iTsnd' 
+        password: 'contraseña' 
     }
 }
 
